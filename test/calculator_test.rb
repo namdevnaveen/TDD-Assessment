5206f7598b062903c5c3dafc6f4e05aba046b905
namdevnaveen <nn17310@gmail.com>
2025-02-17 18:21:46 +0530

    Add test case for one number in input

--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -7,4 +7,8 @@ class CalculatorTest < Minitest::Test
   def test_null_string
     assert_equal 0, Calculator.add('')
   end
+
+  def test_return_one_number
+    assert_equal 5, Calculator.add('5')
+  end
 end
