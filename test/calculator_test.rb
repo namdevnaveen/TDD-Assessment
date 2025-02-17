8e8d8302a909da3934a6f3216b283b29a99b3601
namdevnaveen <nn17310@gmail.com>
2025-02-17 18:23:21 +0530

    Add test for one digit, two digit sum

--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -8,7 +8,15 @@ class CalculatorTest < Minitest::Test
     assert_equal 0, Calculator.add('')
   end
 
-  def test_return_one_number
+  def test_one_digit
     assert_equal 5, Calculator.add('5')
+  end
+
+  def test_one_non_digit
+    assert_equal 0, Calculator.add(',')
+  end
+
+  def test_sum_two_digits
+    assert_equal 6, Calculator.add('2,3')
   end
 end
