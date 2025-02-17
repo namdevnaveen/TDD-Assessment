59da09ad5ed80b3131bb076b423ecc9f482a620e
namdevnaveen <nn17310@gmail.com>
2025-02-17 18:38:39 +0530

    Add test case for single negative integer input

--- calculator.rb
+++ calculator.rb
@@ -1,27 +1,37 @@
 class Calculator
-  def self.add(numbers)
-    if numbers == ""
-      return 0
-    elsif numbers.length == 1
-      return numbers.to_i
-    elsif numbers.length == 3
-      numbers.split(',').map(&:to_i).sum
-    else
-      delimiters = %w[, \n // ;]
-      digits = numbers.split(Regexp.union(delimiters)).map(&:to_i)
+  attr_reader :numbers
+  DELIMITERS = %w[, \n // ;]
 
-      negative_numbers = check_for_negative_number(digits)
-
-      raise "negative numbers not allowed: #{negative_numbers.join(',')}" if negative_numbers.is_a?(Array)
-
-      digits.delete(0)
-
-      return "invalid" if digits.length == 1
-      digits.sum
-    end
+  def initialize(str)
+    @numbers = str
   end
 
-  def self.check_for_negative_number(arr)
+  def add
+    return 0 if @numbers == ""
+    return @numbers.to_i if @numbers.length == 1
+
+    digits = extract_digits
+    handle_negative_numbers(digits)
+
+    return "invalid" if digits.length == 1
+    digits.sum
+  end
+
+  private
+
+  def extract_digits
+    arr = @numbers.split(Regexp.union(DELIMITERS)).map(&:to_i)
+    arr.delete(0)
+    arr
+  end
+
+  def handle_negative_numbers(digits)
+    negative_numbers = check_for_negative_number(digits)
+
+    raise "negative numbers not allowed: #{negative_numbers.join(',')}" if negative_numbers.is_a?(Array)
+  end
+
+  def check_for_negative_number(arr)
     negative_numbers = []
     arr.each do |num|
       negative_numbers << num if num < 0urn digits.sum
--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -5,57 +5,62 @@ require './calculator.rb'
 # CalculatorTest
 class CalculatorTest < Minitest::Test
   def test_null_string
-    assert_equal 0, Calculator.add('')
+    assert_equal 0, Calculator.new('').add
   end
 
   def test_one_digit
-    assert_equal 5, Calculator.add('5')
+    assert_equal 5, Calculator.new('5').add
   end
 
   def test_one_non_digit
-    assert_equal 0, Calculator.add(',')
+    assert_equal 0, Calculator.new(',').add
   end
 
   def test_sum_two_digits
-    assert_equal 6, Calculator.add('2,4')
+    assert_equal 6, Calculator.new('2,4').add
   end
 
   def test_sum_leading_delimiter
-    assert_equal "invalid", Calculator.add(',,4')
+    assert_equal "invalid", Calculator.new(',,4').add
   end
 
   def test_sum_trailing_delimiter
-    assert_equal "invalid", Calculator.add('4,,')
+    assert_equal "invalid", Calculator.new('4,,').add
   end
 
   def test_sum_no_digit
-    assert_equal 0, Calculator.add('/,,')
+    assert_equal 0, Calculator.new('/,,').add
   end
 
   def test_input_with_blank
-    assert_equal 0, Calculator.add('   ')
+    assert_equal 0, Calculator.new('   ').add
   end
 
   def test_input_with_more_digits
-    assert_equal 15, Calculator.add('1,2,3,4,5')
+    assert_equal 15, Calculator.new('1,2,3,4,5').add
   end
  
   def test_invalid_input
-    assert_equal "invalid", Calculator.add('1,\n')
+    assert_equal "invalid", Calculator.new('1,\n').add
   end
  
   def test_different_delimiter
-    assert_equal 3, Calculator.add('//;\n1;2')
+    assert_equal 3, Calculator.new('//;\n1;2').add
+  end
+
+  def test_single_negative_number_input
+    exception = assert_raises(RuntimeError) { Calculator.new('-2').add }
+    assert_equal("negative numbers not allowed: -2", exception.message)
   end
 
   def test_one_negative_number_input
-    assert_raises(RuntimeError) { Calculator.add('1,-2') }
-    exception = assert_raises(RuntimeError) { Calculator.add('1,-2') }
+    assert_raises(RuntimeError) { Calculator.new('1,-2') }
+    exception = assert_raises(RuntimeError) { Calculator.new('1,-2').add }
     assert_equal("negative numbers not allowed: -2", exception.message)
   end
 
   def test_multiple_negative_number_input
-    exception = assert_raises(RuntimeError) { Calculator.add('1,-4,5,-23,-2') }
+    exception = assert_raises(RuntimeError) { Calculator.new('1,-4,5,-23,-2').add }
     assert_equal("negative numbers not allowed: -4,-23,-2", exception.message)
 
   end
